efa2b49dd415be7bcf6775e20f190699717aeab0
themalonaz <malonazria@gmail.com>
2018-01-25 16:28:23 +0000

    implemented choose best decision attribute and its helper functions

--- Data/loadmatlabdata.py
+++ Data/loadmatlabdata.py
@@ -1,9 +1,129 @@
 import scipy.io as spio
+import math
 
-
+# load raw data
 mat = spio.loadmat('cleandata_students.mat', squeeze_me=True)
 
-y = mat['y'] # contains a list of integers
-x = mat['x'] # contains a list of list of integers
+# extract labels and examples from raw data
+labels = mat['y'] # N labels
+examples = mat['x'] # N examples of 45 attributes each.
 
 
+def entropy(positive_count, negative_count):
+    """ Returns the entropy of the positive and negative counts """
+
+    # get the proportion of negative and positive examples
+    pos_proportion = float(positive_count)/(positive_count + negative_count)
+    neg_proportion = float(negative_count)/(positive_count + negative_count)
+    
+    return -pos_proportion*math.log(pos_proportion, 2) - neg_proportion*math.log(neg_proportion, 2)
+
+def information_remainder(examples, attribute_index, binary_targets):
+    """ Returns the information gained by classifying the examples on the given attribute.
+    @params:
+        examples: the examples we wish to classify
+        attribute_index: index of the attribute used to split the data
+        binary_targets: contains binary target for each example, where a 1 indicates the example
+                        is a positive match.
+    """
+
+    # format: [num of examples that are a class match, num of examples that are not]
+    pos_attribute = [0, 0]
+    neg_attribute = [0, 0]
+    
+    for i in range(len(examples)):
+        # get example
+        example = examples[i]
+        
+        if (example[attribute_index] == 1):
+            # example has attribute.
+            pos_attribute[binary_targets[i]] +=1
+            
+        else:
+            # example does not have attribute
+            neg_attribute[binary_targets[i]] += 1
+    
+    # get entropy of examples with positive attribute
+    pos_entropy = entropy(pos_attribute[0], pos_attribute[1])
+    neg_entropy = entropy(neg_attribute[0], neg_attribute[1])
+
+    # get total positive and total negative attributes
+    pos_count = float(sum(pos_attribute))
+    neg_count = float(sum(neg_attribute))
+
+    # get proportion of positive attributes
+    pos_proportion = pos_count/len(examples)
+    neg_proportion = neg_count/len(examples)
+
+    # return remainder entropy
+    return pos_proportion*pos_entropy + neg_proportion*neg_entropy
+
+
+def choose_best_decision_attribute(examples, attributes, binary_targets):
+    """ Returns the index of the best decision attribute to classify the examples on.
+    @params:
+        examples: the examples we wish to classify
+        attributes: attributes to consider
+        binary_targets: contains binary target for each example, where a 1 indicates the example
+                        is a positive match.
+    """
+
+    # calculate initial entropy
+    pos_targets = sum(binary_targets)
+    neg_targets = len(binary_targets) - pos_targets
+    initial_entropy = entropy(pos_targets, neg_targets)    
+
+
+    # find the attribute with the smallest information_remainder
+    min_remainder = information_remainder(examples, 0, binary_targets)
+    min_remainder_attribute = 0
+
+        
+    for i in range(1, len(attributes)):
+        # get remainder for this attribute
+        current_remainder = information_remainder(examples, i, binary_targets)
+        
+        if current_remainder < min_remainder:
+            # update remainder info
+            min_remainder = current_remainder
+            min_remainder_attribute  = i
+
+    return min_remainder_attribute
+    
+# test
+binary_targets = map(lambda x: int(x == 1), labels)
+
+print choose_best_decision_attribute(examples, range(45), binary_targets)
+
+
+
+'''
+def create_binary(attribute_vector, emotion_number):
+    bynary_target = []
+    for elm in attribute_vector:
+        if elm == emotion_number:
+            binary_target.append(1)
+        else:
+            bynary_target.append(0)
+
+return binary_target
+
+
+
+def decision_tree_klearning(examples,attributes,binary_target):
+    if same_value(binary_target):
+        return
+    else
+
+
+
+def same_value(tree):
+    value = tree[0]
+    for elm in tree:
+        if elm != value:
+            return false
+        else:
+            return true
+    
+    
+'''
