d91b03beb5c0ef26991dcc11f490613aab7806be
themalonaz <malonazria@gmail.com>
2018-01-25 18:47:37 +0000

    moved helper files to common

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,6 @@
 import scipy.io as spio
 import math
-
+from common import *
 
 ##### PART I: LOADING DATA
 def load_data(filename):
@@ -29,60 +29,6 @@ def get_binary_targets(labels, label_index):
     
 
 ##### PART II: CREATING DECISION TREE
-def entropy(positive_count, negative_count):
-    """ Returns the entropy of the positive and negative counts """
-
-    # get the proportion of negative and positive examples
-    pos_proportion = float(positive_count)/(positive_count + negative_count)
-    neg_proportion = float(negative_count)/(positive_count + negative_count)
-    
-    return -pos_proportion*math.log(pos_proportion, 2) - neg_proportion*math.log(neg_proportion, 2)
-
-
-def information_remainder(examples, attribute_index, binary_targets):
-    """ Returns the information gained by classifying the examples on the given attribute.
-    @params:
-        examples: the examples we wish to classify
-        attribute_index: index of the attribute used to split the data
-        binary_targets: contains binary target for each example, where a 1 indicates the example
-                        is a positive match.
-    """
-
-    # format: [num of examples that are a class match, num of examples that are not]
-    pos_attribute = [0, 0]
-    neg_attribute = [0, 0]
-    
-    for i in range(len(examples)):
-        # get example
-        example = examples[i]
-        
-        if (example[attribute_index] == 1):
-            # example has attribute.
-            pos_attribute[binary_targets[i]] +=1
-            
-        else:
-            # example does not have attribute
-            neg_attribute[binary_targets[i]] += 1
-
-
-    # get entropy of examples with positive attribute
-    pos_entropy = entropy(pos_attribute[0], pos_attribute[1])
-    neg_entropy = entropy(neg_attribute[0], neg_attribute[1])
-
-    # get total positive and total negative attributes
-    pos_count = float(sum(pos_attribute))
-    neg_count = float(sum(neg_attribute))
-
-    # get proportion of positive attributes
-    pos_proportion = pos_count/(pos_count + neg_count)
-    neg_proportion = neg_count/(pos_count + neg_count)
-    
-    # debug print
-    #print "attribute " + str(attribute_index) + ": " + str(pos_attribute) + ", " + str(neg_attribute) + ", " + \
-    #    str(pos_proportion*pos_entropy + neg_proportion*neg_entropy)
-    
-    # return remainder entropy
-    return pos_proportion*pos_entropy + neg_proportion*neg_entropy
 
 
 def choose_best_decision_attribute(examples, attributes, binary_targets):
@@ -117,6 +63,8 @@ def choose_best_decision_attribute(examples, attributes, binary_targets):
     return min_remainder_attribute
 
 
+
+##### TESTING
 def test_choose_best_decision_attribute():
 
     # get matlab data
@@ -127,6 +75,7 @@ def test_choose_best_decision_attribute():
 
     # find index of best attribute
     print choose_best_decision_attribute(examples, range(45), binary_targets)
+
 
 # call to test
 test_choose_best_decision_attribute()
---
+++ common.py
@@ -0,0 +1,60 @@
+import scipy.io as spio
+import math
+
+##### HELPER FUNCTIONS
+def entropy(positive_count, negative_count):
+    """ Returns the entropy of the positive and negative counts """
+
+    # get the proportion of negative and positive examples
+    pos_proportion = float(positive_count)/(positive_count + negative_count)
+    neg_proportion = float(negative_count)/(positive_count + negative_count)
+    
+    return -pos_proportion*math.log(pos_proportion, 2) - neg_proportion*math.log(neg_proportion, 2)
+
+
+def information_remainder(examples, attribute_index, binary_targets):
+    """ Returns the information gained by classifying the examples on the given attribute.
+    @params:
+        examples: the examples we wish to classify
+        attribute_index: index of the attribute used to split the data
+        binary_targets: contains binary target for each example, where a 1 indicates the example
+                        is a positive match.
+    """
+
+    # format: [num of examples that are a class match, num of examples that are not]
+    pos_attribute = [0, 0]
+    neg_attribute = [0, 0]
+    
+    for i in range(len(examples)):
+        # get example
+        example = examples[i]
+        
+        if (example[attribute_index] == 1):
+            # example has attribute.
+            pos_attribute[binary_targets[i]] +=1
+            
+        else:
+            # example does not have attribute
+            neg_attribute[binary_targets[i]] += 1
+
+
+    # get entropy of examples with positive attribute
+    pos_entropy = entropy(pos_attribute[0], pos_attribute[1])
+    neg_entropy = entropy(neg_attribute[0], neg_attribute[1])
+
+    # get total positive and total negative attributes
+    pos_count = float(sum(pos_attribute))
+    neg_count = float(sum(neg_attribute))
+
+    # get proportion of positive attributes
+    pos_proportion = pos_count/(pos_count + neg_count)
+    neg_proportion = neg_count/(pos_count + neg_count)
+    
+    # debug print
+    #print "attribute " + str(attribute_index) + ": " + str(pos_attribute) + ", " + str(neg_attribute) + ", " + \
+    #    str(pos_proportion*pos_entropy + neg_proportion*neg_entropy)
+    
+    # return remainder entropy
+    return pos_proportion*pos_entropy + neg_proportion*neg_entropy
+
+
