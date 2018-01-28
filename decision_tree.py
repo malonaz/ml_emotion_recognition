991a2edefce4ef1cab9e0d96c980a5feae773c65
themalonaz <malonazria@gmail.com>
2018-01-28 00:25:30 +0000

    implemented the generatation of graph dot file

--- decision_tree.py
+++ decision_tree.py
@@ -11,21 +11,20 @@ class DecisionTree:
                         of examples.
     """
 
+    # static member to keep count of number of nodes
+    node_count = 0
+    
     def __init__(self, test = None, class_label = None):
         """ initializes the decision tree """
         
         self.test = test
         self.kids = []
         self.class_label = class_label
-        self.label = None
 
-
-    def set_label(self, label):
-        """ set this label to the given label. """
-
-        self.label = label
-
-
+        # set id, then increment the static node_count
+        self.id = DecisionTree.node_count
+        DecisionTree.node_count += 1
+        
     def add_kid(self, kid):
         """ adds a kid to the kids field. kid is a DecisionTree."""
 
@@ -35,18 +34,62 @@ class DecisionTree:
     def __str__(self):
         """ overrides the str operator for a decision tree."""
 
-        if self.test:
-            return str(self.test)
+        if self.test != None:
+            return "\"test: " + str(self.test) + "\""
+            
+        return "\"class: " + str(self.class_label) + "\""
+
+    
+    def get_id(self):
+        """ Returns this node's id."""
         
-        return str(self.class_label)
+        return str(self.id)
 
+    def get_label(self):
+        """ Returns this node's label for graphing purposes."""
+        
+        return "[label=" + str(self) + "]"        
 
+    
     def generate_graph(self):
         """ generates a dot file representing this tree."""
 
+        graph_type = "digraph"
+        name = "tree_of_class_"
+        setup = "node [shape=box, font=Courier]\n"
+        code = self.get_edges()
+
+        # put text together
+        text = "%s %s{\n\n%s\n%s\n}" %(graph_type, name, setup, code)
+        
         # open file, write code into it, then close it
         f = open("graph.dot", "w")
-        f.write(get_graph(self))
+        f.write(text)
         f.close()
 
+    
+    def get_edges(self):
+        """ returns text declaring the nodes and their edges in dot format."""
         
+        if (len(self.kids) != 0):
+            # node is not a leaf
+            
+            node = self.get_id() + self.get_label() + "\n"
+            node_left = self.kids[0].get_id() + self.kids[0].get_label() + "\n"            
+            node_right = self.kids[1].get_id() + self.kids[1].get_label() + "\n"
+            
+            edge1 = self.get_id() + " -> " + self.kids[0].get_id() + "\n"
+            edge2 = self.get_id() + " -> " + self.kids[1].get_id() + "\n"
+
+            rest = self.kids[0].get_edges() + self.kids[1].get_edges()
+            
+            if (self.id == 0):
+                return node + node_left + node_right + edge1 + edge2 + rest
+            else:
+                return node_left + node_right + edge1 + edge2 + rest
+            
+
+        return ""
+
+    
+
