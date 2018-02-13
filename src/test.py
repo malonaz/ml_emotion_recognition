70dc424b0415a99cfb8d0fda2ea180e41c0b7104
themalonaz <malonazria@gmail.com>
2018-02-13 11:37:41 +0000

    fixed bug

--- src/test.py
+++ src/test.py
@@ -16,7 +16,7 @@ def main(dataset = "data/cleandata_students.mat", folder = "test_dataset"):
     trained_clean_trees, trained_noisy_trees = load_all_trees()
 
     # 3. get prediction vector using test_trees
-    predictions = test_trees(trained_trees, examples)
+    predictions = test_trees(trained_clean_trees, examples)
 
     # 4. get average error rate
     average_error_rate  = get_error_rate(predictions, labels)
