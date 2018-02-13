160a97e847f811081eb24fc504ece05cc7caa878
themalonaz <malonazria@gmail.com>
2018-02-13 11:30:00 +0000

    removed cross validation

--- src/test.py
+++ src/test.py
@@ -9,31 +9,19 @@ def main(dataset = "data/cleandata_students.mat", folder = "test_dataset"):
     """ dataset should be the filepath to the dataset as observed from root directory.
         Do not change the folder. """
     
-    # 1. get dataset
+    # 1. get dataset. dataset must be the filepath to the dataset as observed from the root directory
     examples, labels = load_data(dataset)
 
-    # 2. train 6 trees on the dataset
-    trained_trees = train_trees(examples, labels)
+    # 2. get our pickled trees
+    trained_trees = load_all_trees()
 
-    # OPTIONAL. if you want to use test trees, here it is. PLease add the data you wish to test the tree on as parameter
-    prediction_vector = test_trees(trained_trees, ADD YOUR EXAMPLES HERE)
+    # 3. get prediction vector using test_trees
+    predictions = test_trees(trained_trees, examples)
+
+    # 4. get average error rate
+    average_error_rate  = get_error_rate(predictions, labels)
     
-    # 3. save trees in pickle format. comment out if you wish to do so
-    save_trees_to_pickle(trained_trees, folder)
-                
-    # 5. generate graphs in (graphs folder) for each tree
-    visualize_trees(trained_trees, folder)
-
-    # 6. perform cross_validation
-    confusion_matrix, average_error_rate = cross_validation(examples, labels)
-
-    # 8. get recall and precision rates
-    recall_rates, precision_rates = get_recall_precision_rates(confusion_matrix)
-
-    # 11. get F_1 measures
-    f1_measures = get_f_measures(recall_rates, precision_rates)
-
-    # 12. get classification rate
+    # 5. get classification rate
     classification_rate = 1 - average_error_rate
 
 
